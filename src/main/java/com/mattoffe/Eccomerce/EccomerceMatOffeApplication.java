19cc83096b831c5a8d5afcb2e89e3bef27941ae6
Bruno Ferreira <fbrunomarcos@gmail.com>
2023-10-07 19:13:51 -0300

    Fix

--- src/main/java/com/mattoffe/Eccomerce/EccomerceMatOffeApplication.java
+++ src/main/java/com/mattoffe/Eccomerce/EccomerceMatOffeApplication.java
@@ -1,13 +1,20 @@
 package com.mattoffe.Eccomerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EccomerceMatOffeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EccomerceMatOffeApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run() {
+		return (args) -> {
+		};
 	}
 
 }
