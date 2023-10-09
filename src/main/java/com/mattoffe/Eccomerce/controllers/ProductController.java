608e039484b562ba3fed246b401a900e824d414f
Bruno Ferreira <fbrunomarcos@gmail.com>
2023-10-08 21:36:57 -0300

    funcionalidades person

--- src/main/java/com/mattoffe/Eccomerce/controllers/ProductController.java
+++ src/main/java/com/mattoffe/Eccomerce/controllers/ProductController.java
@@ -1,4 +1,51 @@
 package com.mattoffe.Eccomerce.controllers;
 
+import com.mattoffe.Eccomerce.dtos.ProductDTO;
+import com.mattoffe.Eccomerce.model.Product;
+import com.mattoffe.Eccomerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+    @GetMapping("/all")
+    public List<ProductDTO> getAllProducts() {
+        return productRepository.findAll().stream().map(ProductDTO::new).collect(toList());
+    }
+    @GetMapping("/{id}")
+    public ProductDTO getOneProduct(@PathVariable Long id) {
+        return new ProductDTO(productRepository.findById(id).orElse(null));
+    }
+    @PatchMapping("/updateStock/{id}")
+    public ResponseEntity<Object> updateStock(@PathVariable Long id, @RequestParam int stock) {
+       if (stock < 0) {
+           return new ResponseEntity<>("Invalid stock", HttpStatus.BAD_REQUEST);
+       }
+        Product product = productRepository.findById(id).orElse(null);
+        product.setStock(product.getStock() + stock);
+        productRepository.save(product);
+        return new ResponseEntity<>("Stock updated", HttpStatus.OK);
+    }
+    @PatchMapping("/updatePrice/{id}")
+    public ResponseEntity<Object> updatePrice(@PathVariable Long id, @RequestParam double price) {
+        Product product = productRepository.findById(id).orElse(null);
+        if (price < 0) {
+            return new ResponseEntity<>("Invalid price", HttpStatus.BAD_REQUEST);
+        }
+        if (price == product.getPrice()) {
+            return new ResponseEntity<>("Price not changed", HttpStatus.OK);
+        }
+        product.setPrice(price);
+        productRepository.save(product);
+        return new ResponseEntity<>("Price updated", HttpStatus.OK);
+    }
 }
--- src/main/java/com/mattoffe/Eccomerce/dtos/ProductDTO.java
+++ src/main/java/com/mattoffe/Eccomerce/dtos/ProductDTO.java
@@ -1,4 +1,74 @@
 package com.mattoffe.Eccomerce.dtos;
 
+import com.mattoffe.Eccomerce.model.Product;
+import com.mattoffe.Eccomerce.model.enums.CatogoryProduct;
+import com.mattoffe.Eccomerce.model.enums.ColorProduct;
+
 public class ProductDTO {
+    private long id;
+    private String name, image;
+    private String description;
+    private int stock;
+    private double price, discount, averagePoints, actuallyTotalPoints;
+    private CatogoryProduct catogoryProduct;
+    private ColorProduct colorProduct;
+
+    public ProductDTO() {
+    }
+    public ProductDTO(Product product){
+        this.name = product.getName();
+        this.image = product.getImage();
+        this.description = product.getDescription();
+        this.stock = product.getStock();
+        this.price = product.getPrice();
+        this.discount = product.getDiscount();
+        this.averagePoints = product.getAveragePoints();
+        this.actuallyTotalPoints = product.getActuallyTotalPoints();
+        this.catogoryProduct = product.getCatogoryProduct();
+        this.colorProduct = product.getColorProduct();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public double getAveragePoints() {
+        return averagePoints;
+    }
+
+    public double getActuallyTotalPoints() {
+        return actuallyTotalPoints;
+    }
+
+    public CatogoryProduct getCatogoryProduct() {
+        return catogoryProduct;
+    }
+
+    public ColorProduct getColorProduct() {
+        return colorProduct;
+    }
 }
